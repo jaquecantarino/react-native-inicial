069f726dde94dfb6a7dec71827b3752144adae75
Jaqueline Cantarino <jaquelinecantarinoc@gmail.com>
2022-11-04 22:26:55 -0300

    lembretes

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, TextInput ,View } from 'react-native';
+import { useState } from 'react';
+
+export default function App(){
+
+  const[lembrete, setLembrete] = useState('')
+  const capturarTexto = (lembreteDigitado) => {
+    lembreteDigitado = lembreteDigitado.toUpperCase()
+    setLembrete(lembreteDigitado)
+  }
+
+  const adicionarLembrete = () => {
+    console.log("Adicionando...", lembrete)
+    setLembrete("")
+  }
 
 return (
-    <View>
-
+    <View style={{padding:40}}>
+      <View>
+        <TextInput 
+          style={{borderBottomColor:"#CCC", borderBottomWidth:2, padding: 12, marginBottom: 4}}
+          placeholder="lembrar..."
+          onChangeText={capturarTexto}
+          value={lembrete}
+        />
+        <Button
+          title='OK'
+          onPress={adicionarLembrete}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
